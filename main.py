ae168ed9e91199a282c4deecea7a4c3e52f38c2a
Jake Casey <jakecasey98@outlook.com>
2020-05-06 21:52:55 +0100

    fixes and exe

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ from PIL import Image
 import warnings
 import webbrowser
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 
 #used to hold all posts
 images=[]
