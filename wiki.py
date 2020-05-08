268f8423fbabfda9179b7adb4930832da3148f5e
Edward Healy <44093797+edwardhealy@users.noreply.github.com>
2020-05-08 16:10:13 +0100

    Add files via upload

--- wiki.py
+++ wiki.py
@@ -1,15 +1,18 @@
 import wikipedia
 import warnings
 
+
 warnings.filterwarnings("ignore")
+
 title = input("What do you want to look up?: ")
 try:
-    page = wikipedia.page(title)
+    page = wikipedia.page(title, auto_suggest=False)
     print(page.summary)
 except:
     topics = wikipedia.search(title)
-    choice = 1
+    print("May refer to: ")
+    for i, topic in enumerate(topics):
+        print(i, topic)
+    choice = int(input("Enter a choice: "))
     assert choice in range(len(topics))
     print(wikipedia.summary(topics[choice]))
-
-
